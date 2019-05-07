630524f436f2707148ef6cf9c0b783718f250400
Nijat <38523713+nijaty@users.noreply.github.com>
2019-05-07 17:48:54 +0400

    Update script.js

--- AJAX_task/js/script.js
+++ AJAX_task/js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(e) {
     $("button").click(function(e) {
         console.log("click");
         $.ajax({
-            url: "https://fake.munisisazade.com/v1/5cd18c1bfd1d61e440b4cac2",
+            url: "https://fake.munisisazade.com/v1/5cd18ca5fd1d61e440b4cac9",
             method: "GET",
             contentType: "application/json",
             success: function(data) {

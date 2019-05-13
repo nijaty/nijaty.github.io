3b02716c52df256d8fefdb61fccffe85a91a55d3
nijaty <nicat.yusubov@gmail.com>
2019-05-13 06:22:14 +0400

    AJAX_taska yenilinme elave etdim

--- AJAX_task/js/script.js
+++ AJAX_task/js/script.js
@@ -1,17 +1,12 @@
-
-console.log("js ishleyir");
-
 $(document).ready(function(e) {
     $("button").click(function(e) {
-        console.log("click");
         $.ajax({
-            url: "https://fake.munisisazade.com/v1/5cd8c0b2fd1d61e440b4d192",
+            url: "https://fake.munisisazade.com/v1/5cd8d428fd1d61e440b4d1dc",
             method: "GET",
             contentType: "application/json",
             success: function(data) {
-                console.log(data);
                 $(data).each(function(index, value){
-                    var record="<tr><td>"+value.name+"</td><td>"+value.albumName+"</td><td>"+value.year+"</td></tr>";
+                    var record="<tr><td>"+value.artistName+"</td><td>"+value.albumName+"</td><td>"+value.albumYear+"</td></tr>";
                     $("table").append(record);
                 });
                     
